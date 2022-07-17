de4830818b573d6051c176ebe5009851c110ad04
Ashwin Prasad <ashwin@onpar.in>
2022-07-18 03:20:29 +0530

    initialized fortune service

---
+++ modules/rewards/fortune_service.go
@@ -0,0 +1,10 @@
+package rewards
+
+// Get a history of the fortune wheel spins for a user
+func GetFortuneHistoryForUser() {}
+
+// Create the next fortune value for a user, based on the previous history
+func CreateFortuneForUser() {}
+
+// Set the fortune factor for a user. The metric which decides how much fortune a user should have
+func SetFortuneFactorForUser() {}
--- modules/users/waitlist_service.go
+++ modules/users/waitlist_service.go
@@ -1,16 +1,43 @@
 package users
 
-// Get all waitlisted users
+// Get all waitlisted users. Mostly used by admins
 func GetWaitlist() {}
 
 // Register a user to the waitlist
 func RegisterToWaitlist() {}
 
 // Get the total waitlist queue count
+// Used to show the total number of users in the queue to a user
 func GetWaitlistQueue() {}
 
-// Get the waitlist queue cardinality for a suer
+// Get the waitlist queue cardinality for a user
+// Show the cardinality of the user in the queue
 func GetWaitlistCardinality() {}
 
-// Allow a user into the application by removing them from the waitlist
-func AllowUser() {}
+// Allow a user into the application by assigning a referral code to them
+func AllowUserByReferralCode() {}
+
+// Allow the user even without a referral code
+// Usually used by other functions or by admin
+func AllowUserRaw() {}
+
+// Set the base queue count to bolster the queue size
+func SetBaseQueueCount() {}
+
+// Email the first N users in the queue
+func EmailFirstNUsersInQueue() {}
+
+// Allow the first N users in the queue
+// Usually couple by emailing them that they are now allowed
+func AllowFirstNUsersInQueue() {}
+
+// Email the last N users in the queue
+func EmailLastNUsersInQueue() {}
+
+// Allow the last N users in the queue
+// Usuallly coupled with emailing them that they are allowed
+func AllowLastNUsersInQueue() {}
+
+// Apply a referral code asssigned to the user
+// Check if the referral code provided by the user, matches the referral code assigned to the user
+func ApplyReferralCode() {}
