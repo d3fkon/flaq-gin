653338393be8eb596bb8cd6062a897ba444e95e3
Ashwin Prasad <ashwin@onpar.in>
2022-07-18 03:06:08 +0530

    inited waitlist service

---
+++ modules/users/waitlist_service.go
@@ -0,0 +1,16 @@
+package users
+
+// Get all waitlisted users
+func GetWaitlist() {}
+
+// Register a user to the waitlist
+func RegisterToWaitlist() {}
+
+// Get the total waitlist queue count
+func GetWaitlistQueue() {}
+
+// Get the waitlist queue cardinality for a suer
+func GetWaitlistCardinality() {}
+
+// Allow a user into the application by removing them from the waitlist
+func AllowUser() {}
